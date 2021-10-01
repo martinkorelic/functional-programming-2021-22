d7b3e81fb9453aca179c366ec27ac735f5099782
Wout van den Berg <wout_van_den_berg@hotmail.comg>
2021-10-01 15:18:29 +0200

    First part of 4.6

--- Assignment 4/AST.hs
+++ Assignment 4/AST.hs
@@ -1,11 +1,19 @@
 module AST where
 
 --start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
+data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Expr Expr -- | VarX
 --or:
 --  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
 --  infixl 6 :+:
 --  infixl 7 :*:
 
+
+
+eval :: (Fractional a) => Expr -> a
+eval (Lit a) = a
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
+eval (Sub a b) = eval a - eval b
+eval (Div a b) = eval a / eval b
 
 --eval :: (Fractional a) => Expr -> a -> Maybe a
