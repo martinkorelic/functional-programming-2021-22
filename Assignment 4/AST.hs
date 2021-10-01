01fff01a8224020ac65d5e8e12ddfb611827af2c
martinkorelic <martin.korelic@protonmail.com>
2021-10-01 21:49:21 +0200

    Assignment 4.6

--- Assignment 4/AST.hs
+++ Assignment 4/AST.hs
@@ -1,19 +1,41 @@
 module AST where
 
 --start with either:
-data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Expr Expr -- | VarX
+data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Expr Expr | VarX
 --or:
 --  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
 --  infixl 6 :+:
 --  infixl 7 :*:
 
-
-
-eval :: (Fractional a) => Expr -> a
-eval (Lit a) = a
-eval (Add a b) = eval a + eval b
-eval (Mul a b) = eval a * eval b
-eval (Sub a b) = eval a - eval b
-eval (Div a b) = eval a / eval b
-
---eval :: (Fractional a) => Expr -> a -> Maybe a
+eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+eval (Lit a) _ = Just (fromInteger a)
+eval VarX x = Just x
+eval (Div a b) 0 = Nothing
+eval (Mul a b) x = case (x1, x2) of 
+                    (Just x1, Just x2) -> Just (x1 * x2)
+                    (Nothing, Just x2) -> Nothing
+                    (Just x1, Nothing) -> Nothing
+                    (Nothing, Nothing) -> Nothing
+                    where x1 = eval a x
+                          x2 = eval b x
+eval (Div a b) x = case (x1, x2) of 
+                    (Just x1, Just x2) -> Just (x1 / x2)
+                    (Nothing, Just x2) -> Nothing
+                    (Just x1, Nothing) -> Nothing
+                    (Nothing, Nothing) -> Nothing
+                    where x1 = eval a x
+                          x2 = eval b x
+eval (Sub a b) x = case (x1, x2) of 
+                    (Just x1, Just x2) -> Just (x1 - x2)
+                    (Nothing, Just x2) -> Nothing
+                    (Just x1, Nothing) -> Nothing
+                    (Nothing, Nothing) -> Nothing
+                    where x1 = eval a x
+                          x2 = eval b x
+eval (Add a b) x = case (x1, x2) of 
+                    (Just x1, Just x2) -> Just (x1 + x2)
+                    (Nothing, Just x2) -> Nothing
+                    (Just x1, Nothing) -> Nothing
+                    (Nothing, Nothing) -> Nothing
+                    where x1 = eval a x
+                          x2 = eval b x
