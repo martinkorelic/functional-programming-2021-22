332d744233d2b9144505e8313d7c409d0723d35b
martinkorelic <martin.korelic@protonmail.com>
2021-10-05 14:56:32 +0200

    Some work on assignment

--- Assignment 5/FunList.hs
+++ Assignment 5/FunList.hs
@@ -1,10 +1,20 @@
 module FunList where
 
---compose :: [a -> a] -> (a -> a)
+compose :: [a -> a] -> (a -> a)
+compose [] x = x
+compose (f:fs) x = f (compose fs x)
 
---compose' :: [a -> a] -> (a -> a)
+compose' :: [a -> a] -> (a -> a)
+compose' f x = foldr (\y -> y) x f
 
---foo :: (Integral n) => n -> n
---foo n = compose (map (*) [1..n]) 1
+foo :: (Integral n) => n -> n
+foo n = compose (map (*) [1..n]) 1
 
---foldr' :: (a -> b -> b) -> b -> [a] -> b
+-- map (*) [1..n] - Partial functions with the first argument of multiplication being a member of the list from 1 to n.
+-- compose takes the list of partial functions which are then folded right with the given argument of "1"
+-- The last of function (n) is then applied the argument 1 and recursively returns it back
+-- Final sequence: 1 * (2 * (3 * (...n)))
+-- Multiplication of first n natural numbers.
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' step h fl = compose (map step fl) h
--- Assignment 5/Unfold.hs
+++ Assignment 5/Unfold.hs
@@ -3,10 +3,21 @@ module Unfold where
 import Data.List (unfoldr)
 import Prelude hiding (take,zip,(++))
 
--- bits :: Int -> [Int]
--- zip :: [a] -> [b] -> [(a,b)]
--- take :: Int -> [a] -> [a]
--- primes :: [Integer]
+bits :: Int -> [Int]
+bits n = unfoldr (\s -> if s == 0 then Nothing else Just (s `mod` 2, s `div` 2)) n
+
+zip :: [a] -> [b] -> [(a,b)]
+zip x y = unfoldr (\s -> if length x > s && length y > s then Just ((x !! s, y !! s), s+1) else Nothing) 0
+
+take :: Int -> [a] -> [a]
+take n x = unfoldr (\s -> if length x > s && n > s then Just (x !! s, s+1) else Nothing) 0
+
+primes :: [Integer]
+primes = unfoldr (\s -> Just (s, nextPrime s 0)) 2
+
+nextPrime :: Integer -> Integer -> Integer
+nextPrime n f   | f /= 0 && length [x | x <- [2 .. n-1], mod n x == 0] == 0 = n
+                | otherwise = nextPrime (n+1) (f+1)
 
 apo :: (t -> Either [a] (a, t)) -> t -> [a]
 apo f seed = case f seed of
