0dcb5f0eeb53d376ecc77191ca806d37d0e0cf00
Wout van den Berg <wout_van_den_berg@hotmail.comg>
2021-10-08 16:18:16 +0200

    exercise 5.8 (except 5.8.4)

--- Assignment 5/WordStats.hs
+++ Assignment 5/WordStats.hs
@@ -1,16 +1,25 @@
 import Data.List
 import Data.Char
 import Data.Function
-import qualified Data.Map as M
+import qualified Data.Map as Map
+
+word :: String
+word = "the eastern spot is the nearest stop in earnest"
 
 wordFrequency :: String -> [(String,Int)]
 wordFrequency  = map (\x->(head x,length x)) . group . sort . words
 
---mostFrequentOfLength :: Int -> String -> ??
+--wordFrequency' :: String -> Map.Map k a
+--wordFrequency' = 
 
---wordLengthFrequency :: String -> ??
+mostFrequentOfLength :: Int -> String -> [(String, Int)]
+mostFrequentOfLength n = reverse . sortOn snd . filter (\(w,l) -> length w >= n) . wordFrequency
 
---anagrams :: String -> ??
+wordLengthFrequency :: String -> [(Int, Int)]
+wordLengthFrequency = sort . map (\x->(length (head x),length x)) . groupBy (\a b->length a == length b) . sortBy (\a b -> compare (length a)  (length b)) . words
+
+anagrams :: String -> [[String]]
+anagrams =  map (map snd) . filter (\l -> length l > 1) . groupBy (\a b -> fst a == fst b) . sort . map (\x -> (sort x, x)) . map head . group . sort . words
 
 main :: IO ()
 main = onStdin $ wordFrequency  -- change this to run a different function
