e95d7d5621935fc987d41c67f5ee65b2682bd34a
martinkorelic <martin.korelic@protonmail.com>
2021-11-16 19:14:09 +0100

    Done 9.5

--- Assignment 9/EditDistance.hs
+++ Assignment 9/EditDistance.hs
@@ -13,11 +13,17 @@ naiveEditDistance xs ys = distance xs ys
   cost x y = if x==y then 0 else 1
 
 
---editDistance :: String -> String -> Int
---editDistance xs ys = ...
---  where
---  ??? :: (Int,Int) -> Int
---
---  distArray :: Array (Int,Int) Int
---  distArray = ...
---
+editDistance :: String -> String -> Int
+editDistance xs ys = distArray ! (max_x, max_y)
+  where
+  max_x = length xs
+  max_y = length ys
+
+  distance :: (Int,Int) -> Int
+  distance (0, ys) = ys
+  distance (xs, 0) = xs
+  distance (xs, ys) = minimum [1 + distArray ! (xs-1, ys), 1 + distArray ! (xs, ys-1), cost xs ys + distArray ! (xs-1, ys-1)]
+  cost x y = if (xs !! (x-1)) == (ys !! (y-1)) then 0 else 1
+
+  distArray :: Array (Int,Int) Int
+  distArray = array ((0,0), (max_x, max_y)) [(ij, distance ij) | i <- [0..max_x], j <- [0..max_y], let ij = (i,j)]
